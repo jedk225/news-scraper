e61f4a51ae6b6d11e054c314935c34274e23d927
jedi225 <jedtk@uci.edu>
2018-10-30 11:23:32 -0700

    updated listen

--- server.js
+++ server.js
@@ -155,6 +155,6 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Start the server
-app.listen(PORT, function () {
+app.listen(process.env.PORT || PORT, function () {
     console.log("App running on port " + PORT);
 });
